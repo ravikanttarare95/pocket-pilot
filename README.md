795d15fc7fd7a1357cddc2fbd75d315da3f4a3e5
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-03 19:15:33 +0530

    Hero Section

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Pocket Pilot helps you manage your income, expenses, and recent transactions in 
 
 > This may take 1-2 minutes to load on first visit since we are using free-plan of onrender to host our backend server. It goes to sleep after 15 minutes of inactivity.
 
-visit the [live demo](http://localhost:5173/) to see it in action!
+visit the [live demo](https://pocket-pilot.onrender.com/) to see it in action!
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import HeroImg from "./assets/hero-img.png";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl text-white bg-slate-900 p-5">Pocket Pilot</h1>
-      <h2 className="text-4xl text-white bg-cyan-600 p-5">Pocket Pilot</h2>
-      <h2 className="text-4xl text-white bg-rose-500 p-5">Pocket Pilot</h2>
+    <div className="min-h-screen flex flex-col text-slate-900 font-serif">
+      {/* Hero Section */}
+      <section className="max-w-7xl mx-auto text-center py-10 px-6 flex-1">
+        <div className="flex justify-center mb-12 max-w-xs mx-auto">
+          <img
+            src={HeroImg}
+            alt="Money Illustration"
+            className="w-full drop-shadow-xl"
+          />
+        </div>
+        <h2
+          className="text-5xl font-bold mb-6 text-slate-800 leading-tight"
+          style={{ fontFamily: '"Cormorant Upright", cursive' }}
+        >
+          Take Control of Your Finances
+        </h2>
+        <p className="text-2xl text-slate-600 mx-auto leading-relaxed">
+          moneyBook helps you budget smarter, spend wiser, and save more with a
+          beautifully simple finance dashboard.
+        </p>
+      </section>
     </div>
   );
 }
-
 export default App;
