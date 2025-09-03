1198abde8a9db0d179f84723ed949c4be5bc3fdf
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-03 15:51:34 +0530

    tailwind Css setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-4xl bg-red-300 p-5">Pocket Pilot</h1>
+    </div>
+  );
 }
 
 export default App;
