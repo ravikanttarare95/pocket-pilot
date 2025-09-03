995240080113a6cac17ee1ac5f9148e332aaffbd
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-03 16:22:10 +0530

    mongoDB setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,6 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGODB_URL);
+  if (conn) {
+    console.log("\n📶 MongoDB connected");
+  }
+};
+
 app.get("/health", (req, res) => {
   res.json({ status: "OK", message: "Server is healthy." });
 });
@@ -15,5 +23,6 @@ app.get("/health", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`\n📞 Server is listening on ${PORT}`);
+  console.log(`\n📞 Server is listening on port ${PORT}`);
+  connectDB();
 });
