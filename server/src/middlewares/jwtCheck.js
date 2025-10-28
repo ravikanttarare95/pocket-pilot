08b7a4b045be97820baaf116e35c2c56cf987794
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-10-28 16:07:17 +0530

    feat:jwtCheck middleware

---
+++ server/src/middlewares/jwtCheck.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+const jwtCheck = (req, res, next) => {
+  req.user = null;
+
+  const { authorization } = req.headers; // Authorization: Bearer token
+
+  if (!authorization) {
+    return res.status(401).json({
+      success: false,
+      message: "Authentication token missing",
+    });
+  }
+  try {
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token. Authentication failed.",
+    });
+  }
+};
+
+export default jwtCheck;
