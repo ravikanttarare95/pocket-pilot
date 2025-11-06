3ed48373e6e210108dba01be8f49207db6fc9258
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-11-06 12:17:33 +0530

    Made Charts Responsive

--- client/src/components/PieChart.jsx
+++ client/src/components/PieChart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PieChart({ chart, chartHeading }) {
   return (
-    <div className="bg-white shadow-md rounded-2xl p-4 transition-transform duration-300 w-[99%] px-2">
+    <div className="bg-white shadow-md rounded-2xl p-4 transition-transform duration-300 w-[99%] sm:max-w-97 px-2">
       {chart}
       <p className="mt-4 text-center text-slate-700 font-medium">
         {chartHeading}
